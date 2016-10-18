de21f14da8707fe1005fe030b170acd66045eb29
roxannegrnt <roxanne.grnt@eduge.ch>
2016-10-18 13:03:06 +0200

    Add files via upload

---
+++ ConnectionDB/ConnectionDB/frmMain.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ConnectionDB
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResult = new System.Windows.Forms.Label();
+            this.tbxID = new System.Windows.Forms.TextBox();
+            this.btnRecherche = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(291, 65);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.TabIndex = 0;
+            this.lblResult.Text = "label1";
+            // 
+            // tbxID
+            // 
+            this.tbxID.Location = new System.Drawing.Point(25, 58);
+            this.tbxID.Name = "tbxID";
+            this.tbxID.Size = new System.Drawing.Size(100, 20);
+            this.tbxID.TabIndex = 1;
+            // 
+            // btnRecherche
+            // 
+            this.btnRecherche.Location = new System.Drawing.Point(162, 58);
+            this.btnRecherche.Name = "btnRecherche";
+            this.btnRecherche.Size = new System.Drawing.Size(75, 23);
+            this.btnRecherche.TabIndex = 2;
+            this.btnRecherche.Text = "Recherche";
+            this.btnRecherche.UseVisualStyleBackColor = true;
+            this.btnRecherche.Click += new System.EventHandler(this.btnRecherche_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 414);
+            this.Controls.Add(this.btnRecherche);
+            this.Controls.Add(this.tbxID);
+            this.Controls.Add(this.lblResult);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox tbxID;
+        private System.Windows.Forms.Button btnRecherche;
+    }
+}
+
---
+++ ConnectionDB/ConnectionDB/frmMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace ConnectionDB
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRecherche_Click(object sender, EventArgs e)
+        {
+            MySqlDataReader reader = null;
+            MySqlConnection sqlConnection1 = new MySqlConnection(@"Server=127.0.0.1;Database=cinema;Uid=cineman;Pwd=cine;");
+            string r = "SELECT titre FROM films WHERE idFilm="+tbxID.Text;
+            MySqlCommand cmd = new MySqlCommand(r,sqlConnection1);
+            sqlConnection1.Open();
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                lblResult.Text = reader[0].ToString();
+
+            }
+
+            sqlConnection1.Close();
+        }
+    }
+}
